1836920d90825b020683877776d3e644430b3db5
Freddie Yang <dark_yty@hotmail.com>
2022-06-23 18:23:15 +1000

    add local storage struture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // hero day
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMM Do"));
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
 // change color code between time
 var currentHour = parseInt(today.format("H"));
@@ -17,6 +17,19 @@ $("textarea").each(function() {
 });
 
 // save to local storage for only a day 
+var notes = {
+    am9: $(9).value,
+    am10: $(10).value,
+    am11: $(11).value,
+    pm12: $(12).value,
+    pm1: $(13).value,
+    pm2: $(14).value,
+    pm3: $(15).value,
+    pm4: $(16).value,
+    pm5: $(17).value,
+}
+
+// clear the local storage on second day
 
 
 
